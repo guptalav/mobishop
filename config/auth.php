20967ec5c7583746a1b6b72d7a3992c29fa84fc6
Lav Gupta <lgupta@parka.ca>
2016-11-05 14:49:48 -0400

    Organize folder structure

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Mobishop\User::class,
+            'model' => Mobishop\Users\User::class,
         ],
 
         // 'users' => [
