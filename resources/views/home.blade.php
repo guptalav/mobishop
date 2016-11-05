59fe5cc20cc140ae075755ed68b880613cee0dab
Lav Gupta <lgupta@parka.ca>
2016-11-05 18:34:15 -0400

    Styles for homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,15 @@
                 @foreach ($products as $product)
                     <li>
                         <a href="#">
-                            {{ Html::image($product->images->first()->path, $product->title, array('class' => 'img-responsive')) }}
+                            @if (!empty($product->images()->first()) && file_exists(public_path($product->images->first()->path)))
+                                {{ Html::image($product->images->first()->path, $product->title, array('class' => 'img-responsive')) }}
+                            @else
+                                {{ Html::image('images/upload/p/default.png', $product->title, array('class' => 'img-responsive')) }}
+                            @endif
                             <h4 class="text-center">{{$product->title}}</h4>
                             <p class="text-center">${{$product->price}}</p>
                         </a>
-                        <p class="text-center"><button type="button" class="btn btn-primary">Add to cart</button></p>
+                        <p class="text-center"><button type="button" class="btn btn-primary">ADD TO CART</button></p>
                     </li>
                 @endforeach
             </ul>
