19653fd137b346af795bbb6858d94e4a273041a4
Lav Gupta <lgupta@parka.ca>
2016-11-06 13:07:09 -0500

    Redirect homepage to products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,12 @@ use Mobishop\Products\Product;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home', compact("products"));
+        return redirect('products');
     }
 }
