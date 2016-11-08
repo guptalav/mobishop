6d13513c2500efca62ff3d6bc2ebb103e84729c5
Lav Gupta <lgupta@parka.ca>
2016-11-07 21:28:43 -0500

    Update products table seed

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -15,7 +15,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 1,
             'title' => 'iPhone 7',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 799.99,
             'slug' => 'iphone-7',
@@ -27,7 +27,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 2,
             'title' => 'iPhone 7 Plus',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 1299.99,
             'slug' => 'iphone-7plus',
@@ -39,7 +39,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 3,
             'title' => 'Samsung Galaxy S7',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 799.99,
             'slug' => 'samsung-s7',
@@ -51,7 +51,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 4,
             'title' => 'Samsung Galaxy Note 3',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 499.99,
             'slug' => 'samsung-note3',
@@ -63,7 +63,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 5,
             'title' => 'Google Pixel',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 799.99,
             'slug' => 'google-pixel',
@@ -75,7 +75,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 6,
             'title' => 'Google Pixel XL',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 999.99,
             'slug' => 'google-pixel-xl',
@@ -87,7 +87,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 7,
             'title' => 'Samsung Galaxy Note 5',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 999.99,
             'slug' => 'samsung-note5',
@@ -99,7 +99,7 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'id' => 8,
             'title' => 'Samsung Galaxy Note 4',
-            'short_description' => '<div> Short description for the product goes here <div>',
+            'short_description' => '<div> Short description for the product goes here </div>',
             'description' => '<p> Long description for the product goes here. </p> <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>',
             'price' => 699.99,
             'slug' => 'samsung-note4',
