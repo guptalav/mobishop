65851af8bf09b95c5c9250309d99cafbf7f6c35e
Lav Gupta <lgupta@parka.ca>
2016-11-09 22:00:37 -0500

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title')
+Not found
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            Sorry, there was an error in processing your request. Please <a href="/">click here</a> to go back to the homepage.
+        </div> 
+    </div>
+</div>
+@endsection
